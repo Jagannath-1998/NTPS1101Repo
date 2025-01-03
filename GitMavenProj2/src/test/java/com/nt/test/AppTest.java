dd36fee36d7f8a5c606b4502d356ac52b02e251a
Jagannath <Jagannath@Jagannath-Sahu>
2025-01-03 18:05:42 +0530

    more test cases added

--- GitMavenProj2/src/test/java/com/nt/test/AppTest.java
+++ GitMavenProj2/src/test/java/com/nt/test/AppTest.java
@@ -14,5 +14,19 @@ public class AppTest {
 		int actual=ar.sum(10, 20);
 		assertEquals(expected, actual);
 	}
+  @Test
+  public void testWithNegatives() {
+		Arithmetic ar = new Arithmetic();
+		int expected=-30;
+		int actual=ar.sum(-10, -20);
+		assertEquals(expected, actual);
+	}
+  @Test
+  public void testWithZero() {
+		Arithmetic ar = new Arithmetic();
+		int expected=0;
+		int actual=ar.sum(0, 0);
+		assertEquals(expected, actual);
+	}
 
 }
