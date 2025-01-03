f11b100d80ce3d1fed3fa5415eb66c4284d88819
Jagannath <Jagannath@Jagannath-Sahu>
2025-01-03 19:06:35 +0530

    minor change in main method

--- GitMavenProj2/src/main/java/com/nt/main/ApplicationRun.java
+++ GitMavenProj2/src/main/java/com/nt/main/ApplicationRun.java
@@ -4,7 +4,6 @@ import com.nt.service.Arithmetic;
 
 public class ApplicationRun {
  public static void main(String[] args) {
-	 
 	 System.out.println("jagannath");
 	Arithmetic ar = new Arithmetic();
 	System.out.println(ar.sum(10, 20));
