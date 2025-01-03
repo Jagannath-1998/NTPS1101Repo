be772ab554e9a8ba14789de15b5ba503e333a5e2
Jagannath <Jagannath@Jagannath-Sahu>
2025-01-03 19:08:08 +0530

    confi

--- GitMavenProj2/src/main/java/com/nt/main/ApplicationRun.java
+++ GitMavenProj2/src/main/java/com/nt/main/ApplicationRun.java
@@ -5,6 +5,7 @@ import com.nt.service.Arithmetic;
 public class ApplicationRun {
  public static void main(String[] args) {
 	Arithmetic ar = new Arithmetic();
+	System.out.println("jagannath sahu");
 	System.out.println(ar.sum(10, 20));
 }
 }
